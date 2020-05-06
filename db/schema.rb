b7d7b43bc4470d55a7694768b7b87f0f7847c75c
junnsp2020 <junnsp2011@yahoo.co.jp>
2020-05-06 18:35:47 +0900

    [Add] カラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_05_05_093018) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.integer "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
